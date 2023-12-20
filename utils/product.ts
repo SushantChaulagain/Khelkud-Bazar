6abf453ed3091a3569c52fec7a3d518bb6185e19
Sushant chaulagain <sushantchaulagain@gmail.com>
2023-12-20 13:08:52 +0545

    worked on product detail page

---
+++ utils/product.ts
@@ -0,0 +1,42 @@
+export const product = {
+    id: "648437b38c44d52b9542e340",
+    name: "Basketball",
+    description:
+      "Official size and weight basketball for professional play and training.",
+    price: 30,
+    brand: "Spalding",
+    category: "Basketball",
+    inStock: true,
+    images: [
+      {
+        color: "Orange",
+        colorCode: "#FFA500",
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/e-shop-vid.appspot.com/o/products%2Fbasketball_orange.png?alt=media&token=8fe19551-173a-4550-9d02-20afffc79b12",
+      },
+    ],
+    reviews: [
+      {
+        id: "6499b4887402b0efd394d8f3",
+        userId: "6499b184b0e9a8c8709821d3",
+        productId: "648437b38c44d52b9542e340",
+        rating: 4,
+        comment:
+          "Great basketball for playing with friends. Good grip and durability.",
+        createdDate: "2023-06-26T15:53:44.483Z",
+        user: {
+          id: "6499b184b0e9a8c8709821d3",
+          name: "Chaoo",
+          email: "example1@gmail.com",
+          emailVerified: null,
+          image:
+            "https://lh3.googleusercontent.com/a/AAcHTtcuRLwWi1vPKaQOcJlUurlhRAIIq2LgYccE8p32=s96-c",
+          hashedPassword: null,
+          createdAt: "2023-06-26T15:40:52.558Z",
+          updatedAt: "2023-06-26T15:40:52.558Z",
+          role: "USER",
+        },
+      },
+    ],
+
+}
