5134539cced202e6e6144d582507b1c8195f84e3
Sushant chaulagain <sushantchaulagain@gmail.com>
2024-01-16 22:32:38 +0545

    Worked on Cart item contents

--- app/cart/ItemContent.tsx
+++ app/cart/ItemContent.tsx
@@ -1,12 +1,45 @@
+'use client';
+
+import { formatPrice } from "@/utils/formatPrice";
 import { CartProductType } from "../product/[productId]/ProductDetails";
+import Link from "next/link";
+import { truncateText } from "@/utils/truncateText";
 
 interface ItemContentProps{
     item: CartProductType
 }
 
 const ItemContent: React.FC<ItemContentProps> = ({item}) => {
-    return ( <div>
-
+    return ( <div className="
+    grid
+    grid-cols-5
+    text-xs
+    md:text-smgap-4
+    border-[1.5px]
+    border-slate-200
+    py-4
+    items-center
+    ">
+        <div className="
+        col-span-2
+        justify-self-start
+        flex
+        gap-2
+        md:gap-4
+        "><Link href={`/product/{item.id}`}>
+            <div></div>
+        </Link>
+        <div className="
+        flex
+        flex-col
+        justify-between
+        ">
+            <Link href={`/product/{item.id}`}>{truncateText(item.name)}</Link>
+        </div>
+        </div>
+        <div>{formatPrice(item.price)}</div>
+        <div></div>
+        <div></div>
     </div> );
 }
  
