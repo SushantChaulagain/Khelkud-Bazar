650a33b22f195ebeef0dc942dbff5d45eb8534c5
Sushant chaulagain <sushantchaulagain@gmail.com>
2024-01-18 12:00:49 +0545

    Worked on removing item from cart

--- app/cart/ItemContent.tsx
+++ app/cart/ItemContent.tsx
@@ -6,12 +6,15 @@ import Link from "next/link";
 import { truncateText } from "@/utils/truncateText";
 import Image from "next/image";
 import SetQuantity from "../components/products/SetQuantity";
+import { useCart } from "@/hooks/useCart";
 
 interface ItemContentProps{
     item: CartProductType
 }
 
 const ItemContent: React.FC<ItemContentProps> = ({item}) => {
+const {handleRemoveProductFromCart} = useCart();
+
     return ( <div className="
     grid
     grid-cols-5
@@ -49,7 +52,7 @@ const ItemContent: React.FC<ItemContentProps> = ({item}) => {
             w-[70px]
             ">
                 <button className="text-slate-500
-                underline" onClick={() => {}}>
+                underline" onClick={() => handleRemoveProductFromCart(item)}>
                     Remove
                 </button>
 
