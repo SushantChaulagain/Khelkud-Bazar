8de9bffd1b93239cd89ecf2d8964169e886f83b9
Sushant chaulagain <sushantchaulagain@gmail.com>
2024-02-21 23:12:14 +0545

    Worked on User menu

--- app/components/nav/NavBar.tsx
+++ app/components/nav/NavBar.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Container from "../Container";
 import { Rubik_Bubbles } from "next/font/google";
 import CartCount from "./CartCount";
+import UserMenu from "./UserMenu";
 
 const rubik = Rubik_Bubbles ({ subsets: ['latin'], weight: ['400'] })
 
@@ -28,7 +29,7 @@ const NavBar = () => {
                 <div className="hidden md:block">Search</div>
                 <div className="flex items-center gap-8 md:gap-12">
                    <CartCount/>
-                    <div>UserMenu</div>
+                    <UserMenu/>
                 </div>
                 </div>
             </Container>
